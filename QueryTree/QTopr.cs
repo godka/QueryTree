870115f04d1312dad751f2d040c03d84a5909973
godka <asdic182@sina.com>
2016-03-18 16:14:37 +0800

    extreme

--- QueryTree/QTopr.cs
+++ QueryTree/QTopr.cs
@@ -7,7 +7,6 @@ namespace test1
 		private List<string> m_collection;
 		public QTopr (){
 			Console.WriteLine ("Created by MythKAst");
-		//	m_collection = new List<string> ();
 		}
 		private void InitQueue(){
 			m_collection = new List<string> ();
@@ -43,6 +42,31 @@ namespace test1
 				Console.WriteLine (m_collection[i]);
 			}
 		}
+        private string[] expand(string str)
+        {
+            string[] ret = { str + "0", str + "1" };
+            return ret;
+        }
+        private void compare_core(string str){
+
+            var ret = Compare(str);
+            switch (ret)
+            {
+                case 0:
+                    Console.WriteLine("{0} -- no response", str);
+                    return;
+                case -1:
+                    Console.WriteLine("{0} -- {1}", str, str);
+                    return;
+                default:
+                    Console.WriteLine("{0} -- collision", str);
+                    foreach (string s in expand(str))
+                    {
+                        compare_core(s);
+                    }
+                    break;
+            }
+        }
 		private int Compare(string s){
 			if (m_collection.Count == 0)
 				return 0;
@@ -63,72 +87,10 @@ namespace test1
 			}
 			return ret;
 		}
-
-		private string ChangeWhileNoResponse(string str){
-			if (str.Equals (""))
-				return string.Empty;
-			int tmp = Convert.ToInt32 (str, 2);
-			tmp++;
-			string tmpstr = Convert.ToString (tmp, 2);
-			for (int i = tmpstr.Length; i < str.Length; i++) {
-				tmpstr = "0" + tmpstr;
-			}
-			if (tmpstr.Length > m_collection [0].Length) {
-				return string.Empty;
-			} else {
-				return tmpstr;
-			}
-		}
-
-		private string ChangeWhileCollision(string str){
-			string tmp = str + "0";
-			if (tmp.Length > m_collection [0].Length) {
-				return string.Empty;
-			} else {
-				return tmp;
-			}
-		}
-
-		private string ChangeWhileCorrect(string str){
-			if (ChangeWhileNoResponse (str).Equals(string.Empty))
-				return string.Empty;
-			if(str.Length == 0)return string.Empty;
-			string tmp = str.Substring (0, str.Length - 1);
-			if (str [str.Length - 1] == '0') {
-				return tmp + "1";
-			} else {
-				return ChangeWhileNoResponse (tmp);
-			}
-		}
-		private void Loop(string status){
-			if (m_collection.Count == 0)
-				return;
-			string tmp = string.Empty;
-			int ret = Compare (status);
-			switch (ret) {
-			case 0:
-				Console.WriteLine ("{0} -- no response", status);
-				tmp = ChangeWhileNoResponse (status);
-				break;
-			case -1:
-				Console.WriteLine ("{0} -- {1}", status, status);
-				tmp = ChangeWhileCorrect (status);
-				break;
-			default:
-				Console.WriteLine ("{0} -- collision",status);
-				tmp = ChangeWhileCollision (status);
-				break;
-			}
-			if (tmp.Equals (string.Empty)) {
-				return;
-			} else {
-				Loop (tmp);
-			}
-			
-		}
 		private void SingleStep(){
 			Console.WriteLine ("=====Start Loop======");
-			Loop ("0");
+			//Loop ("0");
+            compare_core("");
 			Console.WriteLine ("=====End Loop======");
 			if (m_collection.Count == 0)
 				Environment.Exit(0);
